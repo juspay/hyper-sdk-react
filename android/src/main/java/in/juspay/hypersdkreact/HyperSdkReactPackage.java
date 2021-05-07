fc96eb62fbbd0d200543417bb504a11ab744aed1
Sri Harsha Chilakapati <sri.harsha@juspay.in>
2021-05-07 22:56:19 +0530

    fix(PICAF-4958): Fixes for concurrency and lifecycle issues

--- android/src/main/java/in/juspay/hypersdkreact/HyperSdkReactPackage.java
+++ android/src/main/java/in/juspay/hypersdkreact/HyperSdkReactPackage.java
@@ -7,22 +7,20 @@ import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class HyperSdkReactPackage implements ReactPackage {
-  @NonNull
-  @Override
-  public List<NativeModule> createNativeModules(@NonNull ReactApplicationContext reactContext) {
-    List<NativeModule> modules = new ArrayList<>();
-    modules.add(new HyperSdkReactModule(reactContext));
-    return modules;
-  }
+    @NonNull
+    @Override
+    public List<NativeModule> createNativeModules(@NonNull ReactApplicationContext reactContext) {
+        return Collections.singletonList(new HyperSdkReactModule(reactContext));
+    }
 
-  @NonNull
-  @Override
-  public List<ViewManager> createViewManagers(@NonNull ReactApplicationContext reactContext) {
-    return Collections.emptyList();
-  }
+    @NonNull
+    @Override
+    @SuppressWarnings("rawtypes")
+    public List<ViewManager> createViewManagers(@NonNull ReactApplicationContext reactContext) {
+        return Collections.emptyList();
+    }
 }
