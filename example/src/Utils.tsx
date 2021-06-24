7b124f8c0476b029f841453f78455f346803fbd2
Harsh Garg <harsh.garg@juspay.in>
2021-06-24 17:53:08 +0530

    revert: removed hyper-sdk-proxy from master branch

--- example/src/Utils.tsx
+++ example/src/Utils.tsx
@@ -3,7 +3,9 @@ import HyperAPIUtils from './API';
 
 const uuidv4 = () => {
   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+    // eslint-disable-next-line no-bitwise
     var r = (Math.random() * 16) | 0,
+      // eslint-disable-next-line no-bitwise
       v = c === 'x' ? r : (r & 0x3) | 0x8;
     return v.toString(16);
   });
