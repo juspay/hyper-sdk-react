bd741239beb171dda95f0a50a1c62dd5cd9be9c9
Yaswanth Polisetti <yaswanth.polisetti@juspay.in>
2022-12-27 16:06:07 +0530

    improvement(PICAF-19220): upgrade sdk version

---
+++ android/src/main/java/in/juspay/hypersdkreact/LogConstants.java
@@ -0,0 +1,8 @@
+package in.juspay.hypersdkreact;
+
+public class LogConstants {
+    final public static String SUBCATEGORY_HYPER_SDK = "hypersdk";
+
+    final public static String LEVEL_INFO = "info";
+    final public static String LEVEL_ERROR = "error";
+}
