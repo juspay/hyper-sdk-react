7d3ccaf1ffdb3bc24aeb4ff2adb316b2072c76e8
subhash <subhash.chandra@juspay.in>
2023-10-17 18:40:44 +0530

    fix: fixed ios build error with RCT_NEW_ARCH_ENABLED enabled

--- ios/HyperSdkReact.h
+++ ios/HyperSdkReact.h
@@ -6,18 +6,11 @@
  */
 
 
-#ifdef RCT_NEW_ARCH_ENABLED
-#import "RNHyperSdkReactSpec.h"
-#import "React/RCTEventEmitter.h"
-
-@interface HyperSdkReact : RCTEventEmitter <NativeHyperSdkReactSpec>
-#else
 #import <React/RCTBridgeModule.h>
 #import <React/RCTEventEmitter.h>
 #import <HyperSDK/HyperSDK.h>
 
 @interface HyperSdkReact : RCTEventEmitter <RCTBridgeModule>
-#endif
 
 #import <HyperSDK/HyperSDK.h>
 @property HyperServices *hyperInstance;
