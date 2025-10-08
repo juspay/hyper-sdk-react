964350cbd2a18c37d530cf5e4b85712f86b15997
Saransh Khandelwal <saransh.k@juspay.in>
2025-10-08 19:49:49 +0530

    New architecture changes added

--- android/src/main/java/in/juspay/hypersdkreact/HyperFragmentViewManager.java
+++ android/src/main/java/in/juspay/hypersdkreact/HyperFragmentViewManager.java
@@ -21,6 +21,7 @@ import com.facebook.react.bridge.ReadableArray;
 import com.facebook.react.common.MapBuilder;
 import com.facebook.react.uimanager.ThemedReactContext;
 import com.facebook.react.uimanager.ViewGroupManager;
+import com.facebook.react.uimanager.annotations.ReactProp;
 
 import org.json.JSONObject;
 
